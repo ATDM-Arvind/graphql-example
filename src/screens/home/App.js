edbd38567c881c929eb6db431be1c952d0c8c26e
Arvind <arvind.kumar@accolitedigital.com>
2024-08-07 19:57:54 +0530

    first and last commit

---
+++ src/screens/home/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+
+import AddUser from './component/AddUser';
+import UserList from './component/UserList';
+import logo from '../../assets/logo.svg';
+import logoApollo from '../../assets/apolo.png';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logoApollo} className="App-logo" alt="logo" />
+        </div>
+        <p>
+          Welcome to Apollo Client with React JS Updated 1.
+        </p>
+      </header>
+      <AddUser/>
+      <UserList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/screens/home/component/UserItem.js
@@ -0,0 +1,34 @@
+import './UserItem.css'
+
+import { DELETE_USER, GET_USERS } from '../../../utility/queries';
+
+import deleteIcon  from '../../../assets/delete.png'
+import { useMutation } from '@apollo/client';
+
+export const ListItem = ({id,name,city})=>{
+
+    const [deleteUser] = useMutation(DELETE_USER, {
+        refetchQueries: [{ query: GET_USERS }]
+    });
+
+    const deleteUserHandle= ()=>{
+        const confirmDelete = window.confirm("Are you sure you want to delete this user?");
+        if (confirmDelete) {
+            deleteUser({ variables: { deleteusersinput:{Id:id} } })
+        }
+    }
+
+
+    return(
+        <div className="container">
+            <div className='left-view'> 
+                <label className="userName">{name}</label>
+                <label style={{textAlign:'left'}}>{city}</label>
+            </div>
+          <div>
+                <img  className='delete-icon' src={deleteIcon} onClick={deleteUserHandle} alt="delete-img"/>
+          </div>
+            
+        </div>
+    )
+}
